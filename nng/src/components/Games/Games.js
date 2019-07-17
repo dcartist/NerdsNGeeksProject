bb033aac3927ce0e7e6d07f25ea36bd50895509d
DCartist <pbannerman13@gmail.com>
2019-07-17 07:43:18 -0400

    Setup Game folder

---
+++ nng/src/components/Games/Games.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Games extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Games;
