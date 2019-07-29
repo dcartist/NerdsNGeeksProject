7de1b7513a988523bb58993466195b12a4895127
DCartist <pbannerman13@gmail.com>
2019-07-28 23:04:08 -0400

    Updates

--- nng/src/components/NerdStatus/Questions.js
+++
@@ -1,5 +0,0 @@
-var questionsOutside = [
-    "1","2","4","6","7"
-]
-
-console.log("this is from the outside")
---
+++ precache-manifest.d343056537e77fb13d7848d57b515271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf138971fdd3cf1d8bce769ea49c2c6",
+    "url": "/nng/index.html"
+  },
+  {
+    "revision": "0a2b842874ead1ae6206",
+    "url": "/nng/static/css/main.d10b73b6.chunk.css"
+  },
+  {
+    "revision": "855e5fc314a59b3f9db6",
+    "url": "/nng/static/js/2.9d35ad4e.chunk.js"
+  },
+  {
+    "revision": "0a2b842874ead1ae6206",
+    "url": "/nng/static/js/main.b9934612.chunk.js"
+  },
+  {
+    "revision": "93623a56283dc46aad2b",
+    "url": "/nng/static/js/runtime~main.bb23199a.js"
+  },
+  {
+    "revision": "e5ee5d338b9a1775e3e2ecd2066fe342",
+    "url": "/nng/static/media/GA.e5ee5d33.png"
+  }
+]);
